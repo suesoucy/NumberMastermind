3d2ce610870d207c470dd03d1af06e470aa02253
Susan Soucy <52015578+suesoucy@users.noreply.github.com>
2025-04-15 08:24:49 -0400

    Create NumberMastermind.py

---
+++ NumberMastermind.py
@@ -0,0 +1,8 @@
+# Number Mastermind Game
+#
+# In this game, the computer randomly selects a 3-digit number with no repeating digits. 
+# The user must try to guess the number, and after each guess, the program gives feedback.
+# 
+# Name:
+# Date:
+#
